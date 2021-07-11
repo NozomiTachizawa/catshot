e5ff3a571786d86799d87909f42954ed301a2a95
nozomitachizawa <mugikko27@gmail.com>
2021-07-11 22:15:05 +0900

    動画

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
     belongs_to :user
     mount_uploader :image, ImageUploader
-    mount_uploader :video, VideoUploader
+    mount_uploader :video, ImageUploader
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
     has_many :comments, dependent: :destroy
