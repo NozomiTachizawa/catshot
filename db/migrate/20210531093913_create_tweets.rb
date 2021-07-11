992a8125bdf8ba06e069fd5987c734c41d5b4eeb
nozomitachizawa <mugikko27@gmail.com>
2021-07-11 22:42:18 +0900

    動画

--- db/migrate/20210531093913_create_tweets.rb
+++ db/migrate/20210531093913_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
       t.text :body
-
+      t.string :video
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_092820) do
+ActiveRecord::Schema.define(version: 2021_06_28_092602) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_092820) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
+    t.string "video"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
@@ -42,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_07_07_092820) do
     t.float "lng"
     t.integer "user_id"
     t.string "spot"
-    t.string "video"
   end
 
   create_table "users", force: :cascade do |t|
